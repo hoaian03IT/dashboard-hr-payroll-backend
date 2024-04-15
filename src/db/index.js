9b043e4b2a88361ccb4141fb481d67d64b583256
hoaian03 <dangtranhoaian8603@gmail.com>
2024-04-15 13:51:52 +0700

    connect db

---
+++ src/db/index.js
@@ -0,0 +1,57 @@
+const sql = require("mssql/msnodesqlv8");
+const mongoose = require("mongoose");
+const mysql = require("mysql2");
+
+async function connectToHRDB() {
+    let conn;
+    const sqlConfig = {
+        server: process.env.SQL_SERVER,
+        user: process.env.SQL_USERNAME,
+        password: process.env.SQL_PASSWORD,
+        database: process.env.SQL_DB,
+        driver: "msnodesqlv8",
+    };
+    try {
+        conn = await new sql.ConnectionPool(sqlConfig).connect();
+    } catch (error) {
+        console.error("Connect to SQL Server failed" + error.message);
+    }
+
+    return conn;
+}
+
+async function connectToPayrollDB() {
+    let conn;
+    try {
+        const mysqlConfig = {
+            host: process.env.MYSQL_SERVER,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DB,
+        };
+        conn = await mysql.createConnection(mysqlConfig);
+        console.log("Connect to MySQL successfully");
+    } catch (error) {
+        console.error("Connect to MySQL failed" + error.message);
+    }
+    return conn;
+}
+
+async function connectToMongoDB() {
+    try {
+        const stringConnection = process.env.MONGODB_STRING_CONNECTION;
+        await mongoose.connect(stringConnection);
+        console.log("Connect to MongoDB successfully");
+    } catch (error) {
+        console.error("Connect to MongoDB failed - " + error.message);
+    }
+}
+
+async function connectDB(app) {
+    await connectToMongoDB(app);
+    const sqlConn = await connectToHRDB();
+    const mySqlConn = await connectToPayrollDB();
+    return { sqlConn, mySqlConn };
+}
+
+module.exports = { connectDB };
