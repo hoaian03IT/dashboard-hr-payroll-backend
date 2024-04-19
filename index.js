cd368320fcbda788bb75d54c6fc81f312cd5f097
Nhat2510 <nhatvovantuan2510@gmail.com>
2024-04-19 16:51:21 +0700

    update Alert

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const { connectSQL, connectMongoDB } = require("./src/db");
+const { connectSQL, connectMongoDB, connectMySQL } = require("./src/db");
 const { router } = require("./src/router");
+const cors = require("cors");
 
 dotenv.config();
 morgan("tiny");
@@ -10,10 +11,20 @@ morgan("tiny");
 const app = express();
 const port = process.env.PORT || 3333;
 
+const corsOption = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOption));
+
 connectMongoDB();
+connectSQL();
+connectMySQL();
 
 router(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
--- src/app/controllers/alert.js
+++ src/app/controllers/alert.js
@@ -1,3 +1,42 @@
-class Alert {}
+const { connectSQL, connectMySQL } = require("../../db");
+
+class Alert {
+  async getHiring(req, res) {
+    try {
+      let { date } = req.query;
+      const conn = await connectSQL();
+      date = new Date(date);
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const sqlString =
+        "select E.EMPLOYMENT_ID,(P.CURRENT_FIRST_NAME+' '+P.CURRENT_MIDDLE_NAME+' '+P.CURRENT_LAST_NAME) AS FULLNAME,E.HIRE_DATE_FOR_WORKING,YEAR(GETDATE())-YEAR(E.HIRE_DATE_FOR_WORKING) AS ANNIVESARY " +
+        "from PERSONAL P INNER JOIN EMPLOYMENT E ON P.PERSONAL_ID=E.PERSONAL_ID " +
+        `where MONTH(E.HIRE_DATE_FOR_WORKING) = ${month} AND DAY(E.HIRE_DATE_FOR_WORKING)= ${day} `;
+      const result = await conn.request().query(sqlString);
+      return res.status(200).json({
+        data: result.recordset,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        title: "Lỗi",
+        message: error.message,
+      });
+    }
+  }
+  async getVacationDate(req, res) {
+    try {
+      const conn = await connectMySQL();
+      const [results, fields] = await conn.query("select * from Employee");
+      return res.status(200).json({
+        data: results,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        title: "Lỗi",
+        message: error.message,
+      });
+    }
+  }
+}
 
 module.exports = new Alert();
