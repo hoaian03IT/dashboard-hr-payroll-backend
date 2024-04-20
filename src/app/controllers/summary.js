c09b7f567bba578a9d384f83f0b278a488f47551
hoaian03 <dangtranhoaian8603@gmail.com>
2024-04-20 10:21:37 +0700

    .

--- src/app/controllers/summary.js
+++ src/app/controllers/summary.js
@@ -1,3 +1,41 @@
-class Summary {}
+const { connectMySQL, connectSQL } = require("../../db");
+
+class Summary {
+    async getTotalEarnings(req, res) {
+        try {
+            const conn = await connectMySQL();
+            let [results, fields] = await conn.query(
+                "select sum(`Paid To Date`) as `Sum Paid To Date`, sum(`Paid Last Year`) as `Sum Paid To Year`from employee"
+            );
+
+            conn.end();
+
+            res.status(200).json({
+                paidToDate: Number(results[0][0]),
+                paidToLastYear: Number(results[0][1]),
+            });
+        } catch (error) {
+            res.status(500).json({
+                title: "Error",
+                message: error.message,
+            });
+        }
+    }
+
+    async getEarningByDepartment(req, res) {
+        try {
+            const conn = await connectSQL();
+            // const [rows] = await conn.request.query();
+            conn.close();
+        } catch (error) {}
+    }
+
+    async getEarningByShareholder(req, res) {
+        try {
+            const conn = await connectSQL();
+            const [rows] = await conn.request.query();
+        } catch (error) {}
+    }
+}
 
 module.exports = new Summary();
--- src/router/summary.router.js
+++ src/router/summary.router.js
@@ -1,4 +1,7 @@
+const summaryController = require("../app/controllers/summary");
 const express = require("express");
 const router = express.Router();
+
+router.get("/earnings-total-duration", summaryController.getTotalEarnings);
 
 module.exports = router;
