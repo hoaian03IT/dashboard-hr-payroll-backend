228d69355a23aa75b10a3cfaa190d993db7e02a8
hoaian03 <dangtranhoaian8603@gmail.com>
2024-05-23 01:02:59 +0700

    vacation days summary

--- src/router/summary.router.js
+++ src/router/summary.router.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/earnings/total", summaryController.getTotalEarnings);
-router.get("/earnings-by", summaryController.getEarningByParts);
+router.get("/earnings-by", summaryController.getEarningByCategory);
 router.get("/benefit-paid", summaryController.getBenefitPaid);
 router.get("/benefit-plans", summaryController.getBenefitPlanSummary);
 router.get("/vacation-days", summaryController.getSummaryVacationDays);
+router.get("/vacation-days-by", summaryController.getVacationDayByCategories);
 router.get("/employment-vacation-day", summaryController.getEmployeeVacationDay);
 router.get("/personnel/total", summaryController.getTotalPersonnel);
 
